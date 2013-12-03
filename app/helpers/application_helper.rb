f06e338684091dae5a32e6a21b41b73451d88f50
Varun Bhartia <vbhartia@mba2014.hbs.edu>
2013-12-03 01:42:55 -0500

    updated course homepage

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,10 @@ module ApplicationHelper
   def devise_mapping
     @devise_mapping ||= Devise.mappings[:user]
   end
+
+  def to_day_of_week_and_date(input_date)
+
+	input_date.to_date.strftime("%A, %B #{input_date.day.ordinalize}, %Y") 
+	
+  end
 end
