d1a8207593ab05eba231e4971a78d927a9819080
Varun Bhartia <vbhartia@mba2014.hbs.edu>
2013-12-06 02:02:10 -0500

    add item

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,4 +17,26 @@ module ApplicationHelper
 	input_date.to_date.strftime("%A, %B #{input_date.day.ordinalize}, %Y") 
 	
   end
+
+  def to_slug(ret)
+    #strip the string
+
+    #blow away apostrophes
+    ret.gsub! /['`]/,""
+
+    # @ --> at, and & --> and
+    ret.gsub! /\s*@\s*/, " at "
+    ret.gsub! /\s*&\s*/, " and "
+
+    #replace all non alphanumeric, underscore or periods with underscore
+     ret.gsub! /\s*[^A-Za-z0-9\.\-]\s*/, '_'  
+
+     #convert double underscores to single
+     ret.gsub! /_+/,"_"
+
+     #strip off leading/trailing underscore
+     ret.gsub! /\A[_\.]+|[_\.]+\z/,""
+
+     ret
+  end
 end
